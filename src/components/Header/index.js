59042ef2a5ea1fe8f404d500255e20ce075d79ac
mfjaconis <matheusjaconis@gmail.com>
2024-09-17 16:40:04 -0300

    criação do header

---
+++ src/components/Header/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useUser } from "../../hooks/UserContext";
+
+import Cart from "../../assets/cart.svg";
+import Person from "../../assets/person.svg";
+import {
+	Container,
+	ContainerLeft,
+	ContainerRight,
+	ContainerText,
+	Line,
+	PageLink,
+} from "./styles";
+
+export function Header() {
+	const { logout } = useUser();
+	const {
+		push,
+		location: { pathname },
+	} = useHistory();
+
+	const logoutUser = () => {
+		logout();
+	};
+
+	return (
+		<Container>
+			<ContainerLeft>
+				<PageLink onClick={() => push("/")} isActive={pathname === "/"}>
+					Home
+				</PageLink>
+				<PageLink
+					onClick={() => push("/produtos")}
+					isActive={pathname.includes("produtos")}
+				>
+					Ver Produtos
+				</PageLink>
+			</ContainerLeft>
+			<ContainerRight>
+				<PageLink onClick={() => push("/carrinho")}>
+					{" "}
+					<img src={Cart} alt="Carrinho" />
+				</PageLink>
+				<Line> </Line>
+				<PageLink>
+					{" "}
+					<img src={Person} alt="Logo-pessoa" />
+				</PageLink>
+				<ContainerText>
+					<p>Olá, Matheus</p>
+					<a href="/login" onClick={logoutUser}>
+						Sair
+					</a>
+				</ContainerText>
+			</ContainerRight>
+		</Container>
+	);
+}
--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ export * from "./Button";
 export * from "./CardProduct";
 export * from "./CartItems";
 export * from "./CategoryCarousel";
+export * from "./Header";
 export * from "./OffersCarousel";
