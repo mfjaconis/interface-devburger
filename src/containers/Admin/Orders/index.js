ed1dc5ac0c7a7059c8c0e59f878b616df753bf82
mfjaconis <matheusjaconis@gmail.com>
2024-09-18 18:23:29 -0300

    alterando o status do pedido dinamicamente

--- src/containers/Admin/Orders/index.js
+++ src/containers/Admin/Orders/index.js
@@ -10,11 +10,14 @@ import TableRow from "@mui/material/TableRow";
 
 import api from "../../../services/api";
 import formatDate from "../../../utils/formatDate";
+import status from "./order-status";
 import Row from "./row";
-import { Container } from "./styles";
+import { Container, LinkMenu, Menu } from "./styles";
 
 function Orders() {
 	const [orders, setOrders] = useState([]);
+	const [filteredOrders, setFilteredOrders] = useState([]);
+	const [activeStatus, setActiveStatus] = useState(1);
 	const [rows, setRows] = useState([]);
 
 	useEffect(() => {
@@ -22,6 +25,7 @@ function Orders() {
 			const { data } = await api.get("orders");
 
 			setOrders(data);
+			setFilteredOrders(data);
 		}
 		loadOrders();
 	}, []);
@@ -37,12 +41,48 @@ function Orders() {
 	}
 
 	useEffect(() => {
-		const newRows = orders.map((ord) => createData(ord));
+		const newRows = filteredOrders.map((ord) => createData(ord));
 		setRows(newRows);
+	}, [filteredOrders]);
+
+	useEffect(() => {
+		if (activeStatus === 1) {
+			setFilteredOrders(orders);
+		} else {
+			const statusIndex = status.findIndex(
+				(status) => status.id === activeStatus,
+			);
+			const newFilteredOrders = orders.filter(
+				(order) => order.status === status[statusIndex].value,
+			);
+			setFilteredOrders(newFilteredOrders);
+		}
 	}, [orders]);
+
+	function handleStatus(status) {
+		if (status.id === 1) {
+			setFilteredOrders(orders);
+		} else {
+			const newOrders = orders.filter((order) => order.status === status.value);
+			setFilteredOrders(newOrders);
+		}
+		setActiveStatus(status.id);
+	}
 
 	return (
 		<Container>
+			<Menu>
+				{status?.map((status) => (
+					<LinkMenu
+						key={status.id}
+						onClick={() => handleStatus(status)}
+						isActiveStatus={activeStatus === status.id}
+					>
+						{status.label}
+					</LinkMenu>
+				))}
+			</Menu>
+
 			<TableContainer component={Paper}>
 				<Table aria-label="collapsible table">
 					<TableHead>
@@ -56,7 +96,12 @@ function Orders() {
 					</TableHead>
 					<TableBody>
 						{rows.map((row) => (
-							<Row key={row.orderId} row={row} />
+							<Row
+								key={row.orderId}
+								row={row}
+								setOrders={setOrders}
+								orders={orders}
+							/>
 						))}
 					</TableBody>
 				</Table>
